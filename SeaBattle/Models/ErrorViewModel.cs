1ac3c9414641d4c41a2b6a10988776be2741bc29
Филиппов Алексей <sickleshaped@gmail.com>
2024-07-05 09:43:04 +0300

    Добавьте файлы проекта.

---
+++ SeaBattle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeaBattle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
